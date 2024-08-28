1890294ba437700bc6ffd01138c00f871d4925ee
kate116 <23-07635@g.batstate-u.edu.ph>
2024-08-28 12:51:01 +0800

    Delete ArellanoKateArbieIT2104_ACPactivities/string-handling.py

--- ArellanoKateArbieIT2104_ACPactivities/string-handling.py
+++
@@ -1,6 +0,0 @@
-c1 = (input("Enter first color: "))
-c2 = (input("Enter second color: "))
-c3 = (input("Enter third color: "))
-
-message = f"{c1}, {c2}, and {c3} created a new color!"
-print(message)
