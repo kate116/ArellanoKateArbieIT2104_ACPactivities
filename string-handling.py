90c9360844b7a1a727ae2101c13affc7d261bf45
kate116 <23-07635@g.batstate-u.edu.ph>
2024-08-28 12:52:41 +0800

    Add files via upload

---
+++ string-handling.py
@@ -0,0 +1,6 @@
+c1 = (input("Enter first color: "))
+c2 = (input("Enter second color: "))
+c3 = (input("Enter third color: "))
+
+message = f"{c1}, {c2}, and {c3} created a new color!"
+print(message)
